93236432156667a7222942c7bc53d36359acbea5
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-05-28 12:03:55 +1000

    end of class update

--- SunnyLand/Assets/Scripts/Player.cs
+++ SunnyLand/Assets/Scripts/Player.cs
@@ -9,10 +9,12 @@ public class Player : MonoBehaviour
     public float gravity = -10f;
 
     private CharacterController2D controller;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,5 +24,14 @@ public class Player : MonoBehaviour
         float inputV = Input.GetAxis("Vertical");
         controller.move(transform.right * inputH * moveSpeed *Time.deltaTime);
        
+    }
+
+    void Move(float inputH, float inputV)
+    {
+        controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
+        bool isRunning = inputH != 0;
+        anim.SetBool("IsRunning", isRunning);
+
+        //rend.flipX = inputH > 0
     }
 }
